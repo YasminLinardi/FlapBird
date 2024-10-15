13a10fd532d180f1dbe669db4dea2f3640ba760d
FlapBird <yasminvitorialinardi@gmail.com>
2024-10-15 11:33:49 -0300

    Pulo e Gravidade

--- FlapBird/MainPage.xaml.cs
+++ FlapBird/MainPage.xaml.cs
@@ -1,11 +1,129 @@
 ﻿namespace FlapBird;
 
 public partial class MainPage : ContentPage
+
 {
-	public MainPage()
+    const int gravidade = 3;
+    const int tempoEntreFrames = 25;
+	const int maxTempoPulando = 3;
+	const int forcaPulo = 60;
+    bool morto = true;
+	double larguraJanela = 0;
+    double alturaJanela = 0;
+    int velocidade = 20;
+	int tempoPulando = 0;
+	bool estaPulando = false;
+	
+
+
+    public MainPage()
+        {
+                InitializeComponent();
+        }
+
+    async Task Desenhar()
+        {
+            while (!morto)
+            	{
+					if (estaPulando)
+					AplicaPulo();
+					else
+					AplicarGravidade();
+                    GerenciaCanos();
+					if (VerificaColisao())
+					{
+						morto = true;
+						frameGameOver.IsVisible = true;
+						break;
+					}
+					await Task.Delay(tempoEntreFrames);
+            	}
+        }
+
+	void AplicaPulo()
 	{
-		InitializeComponent();
+		Slime.TranslationY -= forcaPulo;
+		tempoPulando++;
+		if(tempoPulando >= maxTempoPulando)
+		{
+			estaPulando = false;
+			tempoPulando = 0;
+		}
 	}
 
+	bool VerificaColisaoteto ()
+		{
+			var minY = alturaJanela/2;
+			if (Slime.TranslationY <= minY)
+			return true;
+			else
+			return false;
+		}
+
+	bool VerificaColisaoChao ()
+	{
+		var maxY = alturaJanela /2	- Chão.HeightRequest;
+		if (Slime.TranslationY >=maxY)
+		return true;
+		else
+		return false;
+	}
+
+	bool VerificaColisao()
+		{
+			if(!morto)
+			{
+				if(VerificaColisaoteto()||
+				VerificaColisaoChao())
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	
+    void AplicarGravidade()
+        {
+            Slime.TranslationY += gravidade;
+        }
+
+    protected override void OnSizeAllocated(double width, double height)
+    	{
+        	base.OnSizeAllocated(width, height);
+                larguraJanela=width;
+                alturaJanela=height;
+    	}
+
+    void GerenciaCanos ()
+        {
+            CanoC.TranslationX-= velocidade;
+            CanoB.TranslationX-= velocidade;
+                if (CanoB.TranslationX<-larguraJanela)
+                	{
+                        CanoB.TranslationX=0;
+                        CanoB.TranslationX=0;
+                	}
+        }
+    void Inicializar()
+        {
+            morto=false;
+            Slime.TranslationY=0;
+        }
+
+    void OnGameOverClicked(object s, TappedEventArgs a)
+        {
+            frameGameOver.IsVisible=false;
+            Inicializar();
+            Desenhar();
+
+        }
+
+	void OnGridClicked(object s, TappedEventArgs a)
+	{
+		estaPulando = true;
+	}
+
+
 }
+
 
