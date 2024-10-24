474cc9acb71b9db3c8f4c670f291401d33de4727
FlapBird <yasminvitorialinardi@gmail.com>
2024-10-24 09:00:24 -0300

    correção

--- FlapBird/MainPage.xaml.cs
+++ FlapBird/MainPage.xaml.cs
@@ -9,18 +9,47 @@ public partial class MainPage : ContentPage
 	const int forcaPulo = 15;
 	const int aberturaMinima = 10;
 	bool morto = true;
+	bool estaPulando = false;
 	double larguraJanela = 0;
 	double alturaJanela = 0;
 	int velocidade = 20;
 	int tempoPulando = 0;
 	int score = 0;
-	bool estaPulando = false;
 
 
 
 	public MainPage()
 	{
 		InitializeComponent();
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+	}
+
+	protected override void OnSizeAllocated(double width, double height)
+	{
+		base.OnSizeAllocated(width, height);
+
+		larguraJanela = width;
+		alturaJanela = height;
+		if (height > 0)
+		{
+			CanoC.HeightRequest = height - Chao.HeightRequest;
+			CanoB.HeightRequest = height - Chao.HeightRequest;
+		}
+	}
+
+	void Inicializar()
+	{
+		morto = false;
+		CanoC.TranslationX = -larguraJanela;
+		CanoC.TranslationY = -larguraJanela;
+		Slime.TranslationX = 0;
+		Slime.TranslationY = 0;
+		score = 0;
+		GerenciaCanos();
 	}
 
 	async Task Desenhar()
@@ -35,6 +64,7 @@ public partial class MainPage : ContentPage
 			if (VerificaColisao())
 			{
 				morto = true;
+				labelGameOver.Text = "Você passou por\n" + score + " canos";
 				frameGameOver.IsVisible = true;
 				break;
 			}
@@ -53,49 +83,35 @@ public partial class MainPage : ContentPage
 		}
 	}
 
-	bool VerificaColisaoteto()
-	{
-		var minY = alturaJanela/2;
-		if (Slime.TranslationY <= minY)
-			return true;
-		else
-			return false;
-	}
-
-	bool VerificaColisaoChao()
-	{
-		var maxY = alturaJanela/2 - Chão.HeightRequest;
-		if (Slime.TranslationY >= maxY)
-			return true;
-		else
-			return false;
-	}
-
 	bool VerificaColisao()
 	{
-		if (!morto)
-		{
-			if (VerificaColisaoteto() ||
-			VerificaColisaoChao() ||
-			VerificaColisaoCanoCima() ||
-			VerificaColisaoCanoBaixo())
-			
-				return true;
-			
-		}
-		return false;
+		    return (!morto && 
+					(VerificaColisaoChao() ||
+			 		VerificaColisaoteto() ||
+			  		VerificaColisaoCano()));
+
 	}
+
+	  bool VerificaColisaoCano()
+  	{
+    if (VerificaColisaoCanoB() || VerificaColisaoCanoC())
+      return true;
+    else
+      return false;
+  	}
+
+	bool VerificaColisaoteto()
+	{
+    var alturaDoTeto = alturaJanela / 2;
+    if (Slime.TranslationY <= -alturaDoTeto)
+      return true;
+    else
+      return false;
+  }
 
 	void AplicarGravidade()
 	{
 		Slime.TranslationY += gravidade;
-	}
-
-	protected override void OnSizeAllocated(double width, double height)
-	{
-		base.OnSizeAllocated(width, height);
-		larguraJanela = width;
-		alturaJanela = height;
 	}
 
 	void GerenciaCanos()
@@ -106,30 +122,23 @@ public partial class MainPage : ContentPage
 		{
 			CanoB.TranslationX = 0;
 			CanoB.TranslationX = 0;
-			var alturaMax = -100;
-			var alturaMin = -CanoB.HeightRequest;
+			var alturaMax = -(CanoB.HeightRequest * 0.1);
+			var alturaMin = -(CanoB.HeightRequest * 0.8);
 			CanoC.TranslationY = Random.Shared.Next((int)alturaMin, (int)alturaMax);
-			CanoB.TranslationY = CanoC.TranslationY + aberturaMinima + CanoB.HeightRequest;
+			CanoB.TranslationY = CanoC.HeightRequest + CanoC.TranslationY + aberturaMinima;
 			score++;
-			labelScore.Text = "Canos:" + score.ToString("D3");
+			labelScore.Text = "Score: " + score.ToString("D5");
+			if (score % 4 == 0)
+			velocidade++;
 		}
-	}
-	void Inicializar()
-	{
-		morto = false;
-		CanoC.TranslationX = -larguraJanela;
-		CanoC.TranslationY = -larguraJanela;
-		Slime.TranslationX = 0;
-		Slime.TranslationY = 0;
-		score = 0;
-		GerenciaCanos();
 	}
 
 	void OnGameOverClicked(object s, TappedEventArgs a)
 	{
 		frameGameOver.IsVisible = false;
-		Inicializar();
-		Desenhar();
+		 Inicializar();
+    	 morto = false;
+    	 Desenhar().RunSynchronously();
 
 	}
 
@@ -138,7 +147,7 @@ public partial class MainPage : ContentPage
 		estaPulando = true;
 	}
 
-	bool VerificaColisaoCanoCima()
+	bool VerificaColisaoCanoC()
 	{
 		var posHSlime = (larguraJanela / 2) - (Slime.WidthRequest / 2);
 		var posVSlime = (alturaJanela / 2) - (Slime.HeightRequest / 2) + Slime.TranslationY;
@@ -151,7 +160,7 @@ public partial class MainPage : ContentPage
 			return false;
 	}
 
-	bool VerificaColisaoCanoBaixo()
+	bool VerificaColisaoCanoB()
 	{
 		var posHSlime = (larguraJanela / 2) - (Slime.WidthRequest / 2);
 		var posVSlime = (alturaJanela / 2) - (Slime.HeightRequest / 2) + Slime.TranslationY;
@@ -164,6 +173,16 @@ public partial class MainPage : ContentPage
 			return false;
 
 
+	}
+
+		bool VerificaColisaoChao()
+	{
+		var maxY = alturaJanela / 2;
+		if (Slime.TranslationY >= alturaDoTeto - Chao.Height)
+		{
+			return true;
+		}
+			return false;
 	}
 
 
